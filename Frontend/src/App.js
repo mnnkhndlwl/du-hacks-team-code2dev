e1b7d6882b022bb5d51f5a9df70c574f58697874
mnnkhndlwl <khandelwalmanan05@gmail.com>
2022-07-16 17:19:10 +0530

    backend

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -1,12 +1,11 @@
 
 import './App.css';
-import Navbar from './components/Navbar/Navbar';
 
 function App() {
   return (
     <div className="min-h-screen"> 
       <div className="gradient-bg-welcome">
-        <Navbar />
+        
       </div>
     </div>
   );
